8d733ea34896a86d0a086e5f873ec0439a127c81
rtinter <rene.tinter@googlemail.com>
2023-06-04 21:38:30 +0200

    gamestate

--- src/Main.java
+++ src/Main.java
@@ -26,8 +26,12 @@ public class Main extends PApplet {
     int score = 0;
 
     int timerStart;
+
     int elapsedSeconds;
 
+    int pauseTime;
+
+    GameState gameState;
 
 
     @Override
@@ -46,73 +50,100 @@ public class Main extends PApplet {
         //Block im gridNextStone zeichnen
         gridNextStone.drawBlock(nextBlockInQueue.getTiles(), nextBlockInQueue.startCoordsForNextBlock(), nextBlockInQueue.getId(), nextBlockInQueue.currentRotation);
 
+        gameState = new GameState(this);
 
+        gameState.startScreen();
     }
 
     @Override
     public void draw() {
 
-        // Update elapsedSeconds by subtracting the timer start time from the current time and dividing by 1000 to get seconds.
-        elapsedSeconds = (millis() - timerStart) / 1000;
-
-        background(255);
-
-        BlockFactory blockFactory = new BlockFactory();
-
-
-        // gridPlayground zeichnen
-        gridPlayground.draw(this);
-
-        // gridNextStone zeichnen
-        translate(380, 0);
-        gridNextStone.draw(this);
-
-
-        if (frameCount % speed == 0) {
-
-            // Löscht den aktuellen Block bevor er sich bewegt
-            gridPlayground.deleteBlock(currentBlock.getTiles(), currentBlock.getCurrentPosition(), currentBlock.currentRotation);
-
-            // Bewegt den Block nach unten
-            boolean movedDown = currentBlock.moveDown(gridPlayground);
-
-            // Überprüft, ob der Block erfolgreich nach unten bewegt wurde
-            if (!movedDown) {
-                // Block konnte nicht nach unten bewegt werden, daher wird er eingefroren
-                currentBlock.freeze(gridPlayground);
-
-                int points = kollision.clearFullRows(gridPlayground); // Punkte zuweisen
-                score += points; // Punkte erhöhen
-
-                gridNextStone.setupGrid(gridNextStone);
-
-                // Erzeugt einen neuen Block
-                currentBlock = nextBlockInQueue;
-
-                // Den darauffolgenden im nextblockfeld anzeigen lassen
-                nextBlockInQueue = blockFactory.getNextBlock();
-                gridNextStone.drawBlock(nextBlockInQueue.getTiles(), nextBlockInQueue.startCoordsForNextBlock(), nextBlockInQueue.getId(), nextBlockInQueue.currentRotation);
-            }
+        if (gameState.getState() == GameState.State.START) {
+            background(255);
+            textSize(60);
+            fill(0);
+            textAlign(CENTER, CENTER);
+            text("TETRIS", width / 2, height / 2 - 40);
+            textSize(24);
+            text("Press 'Enter' to begin", width / 2, height / 2 + 20);
         }
+
+        //
+        if (gameState.getState() == GameState.State.RUNNING) {
+
+
+            // Update elapsedSeconds by subtracting the timer start time from the current time and dividing by 1000 to get seconds.
+            elapsedSeconds = (millis() - timerStart) / 1000;
+
+            background(255);
+
+            BlockFactory blockFactory = new BlockFactory();
+
+
+            // gridPlayground zeichnen
+            gridPlayground.draw(this);
+
+            // gridNextStone zeichnen
+            pushMatrix(); //Speichern des aktuellen Matrix-Zustands (sonst verschiebt sich die Score/Timer Anzeige)
+            translate(380, 0);
+            gridNextStone.draw(this);
+            popMatrix(); //Wiederherstellen des letzen Matrix-Zustands (sonst verschiebt sich die Score/Timer Anzeige)
+
+
+            if (frameCount % speed == 0) {
+
+                // Löscht den aktuellen Block bevor er sich bewegt
+                gridPlayground.deleteBlock(currentBlock.getTiles(), currentBlock.getCurrentPosition(), currentBlock.currentRotation);
+
+                // Bewegt den Block nach unten
+                boolean movedDown = currentBlock.moveDown(gridPlayground);
+
+                // Überprüft, ob der Block erfolgreich nach unten bewegt wurde
+                if (!movedDown) {
+                    // Block konnte nicht nach unten bewegt werden, daher wird er eingefroren
+                    currentBlock.freeze(gridPlayground);
+
+                    int points = kollision.clearFullRows(gridPlayground); // Punkte zuweisen
+                    score += points; // Punkte erhöhen
+
+                    gridNextStone.setupGrid(gridNextStone);
+
+                    // Erzeugt einen neuen Block
+                    currentBlock = nextBlockInQueue;
+
+                    // Den darauffolgenden im nextblockfeld anzeigen lassen
+                    nextBlockInQueue = blockFactory.getNextBlock();
+                    gridNextStone.drawBlock(nextBlockInQueue.getTiles(), nextBlockInQueue.startCoordsForNextBlock(), nextBlockInQueue.getId(), nextBlockInQueue.currentRotation);
+                }
+            }
 
 
             // Zeichnet den Block nach jedem Tick neu
-        gridPlayground.drawBlock(currentBlock.getTiles(), currentBlock.getCurrentPosition(), currentBlock.getId(), currentBlock.currentRotation);
+            gridPlayground.drawBlock(currentBlock.getTiles(), currentBlock.getCurrentPosition(), currentBlock.getId(), currentBlock.currentRotation);
 
-        // Zeigt den Punktestand
-        fill(0);
-        textSize(24);
-        text("Score: " + score, 40, 300);
+            // Zeigt den Punktestand
+            fill(0);
+            textSize(24);
+            text("Score: " + score, 480, 300); //x-Koordinate verändert, da sie nach Spielstart verschoben wurde
 
-        //Zeigt die vergangene Zeit
-        fill(0);
-        textSize(24);
-        text("Time: " + elapsedSeconds, 40, 350);
-      }
+            //Zeigt die vergangene Zeit
+            fill(0);
+            textSize(24);
+            text("Time: " + elapsedSeconds, 480, 350); //x-Koordinate verändert, da sie nach Spielstart verschoben wurde
+        }
+        // Bei Pausierung: "PAUSE"
+        else if (gameState.getState() == GameState.State.PAUSED) {
+            //background(255);
+            textSize(60);
+            fill(255, 0, 0);
+            textAlign(CENTER, CENTER);
+            text("PAUSE", width / 2, height / 2);
+        }
+    }
 
 
     public void keyPressed() {
-        switch(keyCode) {
+        switch (keyCode) {
             case LEFT: // Bewegt den Block nach links, wenn die linke Pfeiltaste gedrückt wird
                 gridPlayground.deleteBlock(currentBlock.getTiles(), currentBlock.getCurrentPosition(), currentBlock.currentRotation);
                 currentBlock.moveLeft(gridPlayground);
@@ -132,7 +163,7 @@ public class Main extends PApplet {
                 break;
 
             case UP:
-                gridPlayground.deleteBlock(currentBlock.getTiles(), currentBlock.getCurrentPosition(),currentBlock.currentRotation);
+                gridPlayground.deleteBlock(currentBlock.getTiles(), currentBlock.getCurrentPosition(), currentBlock.currentRotation);
                 currentBlock.rotate(gridPlayground);
                 gridPlayground.drawBlock(currentBlock.getTiles(), currentBlock.getCurrentPosition(), currentBlock.getId(), currentBlock.currentRotation);
                 break;
@@ -140,8 +171,35 @@ public class Main extends PApplet {
             default:
                 break;
         }
+
+        // Tasteneingabe um das Spiel zu starten
+        if (key == ENTER) {
+            if (gameState.getState() == GameState.State.START) {
+                gameState.startGame(); // Spiel starten, wenn es gestartet wird
+            }
+        }
+
+        //Tasteneingabe zum pausieren des Spiels
+        if (key == ' ') { // Wenn die Leertaste gedrückt wird
+            if (gameState.getState() == GameState.State.RUNNING) {
+                gameState.pauseGame(); // Spiel pausieren, wenn es läuft
+            } else if (gameState.getState() == GameState.State.PAUSED) {
+                gameState.resumeGame(); // Spiel fortsetzen, wenn es pausiert ist
+            }
+        }
     }
 
+    //Für GameState
+    public int getScore() {
+        return this.score;
+    }
 
+    public int getElapsedTime() {
+        return this.elapsedSeconds;
+    }
+
+    public void resetTimerStart() {
+        this.timerStart = millis();
+    }
 }
 
